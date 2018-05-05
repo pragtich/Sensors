c5fff5e071cdac96f7b985886d6113b4d5565c10
Joris Pragt <pragtich@gmail.com>
2018-05-05 20:44:46 +0200

    Config for my radios

--- gw-stm32/src/GatewaySerial.ino
+++ gw-stm32/src/GatewaySerial.ino
@@ -38,17 +38,21 @@
 
 // Enable debug prints to serial monitor
 #define MY_DEBUG
-
+#define MY_DEBUG_VERBOSE
 
 // Enable and select radio type attached
-#define MY_RADIO_NRF24
+//#define MY_RADIO_NRF24
 //#define MY_RADIO_NRF5_ESB
-//#define MY_RADIO_RFM69
+#define MY_RADIO_RFM69
+#define MY_RFM69_FREQUENCY RFM69_868MHZ
+#define MY_IS_RFM69HW
+#define MY_RFM69_NEW_DRIVER
+#define MY_DEBUG_VERBOSE_RFM69
 //#define MY_RADIO_RFM95
 
 // Set LOW transmit power level as default, if you have an amplified NRF-module and
 // power your radio separately with a good regulator you can turn up PA level.
-#define MY_RF24_PA_LEVEL RF24_PA_LOW
+//#define MY_RF24_PA_LEVEL RF24_PA_LOW
 
 // Enable serial gateway
 #define MY_GATEWAY_SERIAL
@@ -59,7 +63,7 @@
 #endif
 
 // Enable inclusion mode
-#define MY_INCLUSION_MODE_FEATURE
+//#define MY_INCLUSION_MODE_FEATURE
 // Enable Inclusion mode button on gateway
 //#define MY_INCLUSION_BUTTON_FEATURE
 
